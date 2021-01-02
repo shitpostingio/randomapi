dce65fb7f2693c3ad962c915f979187db1e059a2
Alessandro Pomponio <pomponio.alessandro@hotmail.it>
2021-01-02 18:32:28 +0100

    Luck-based FindRandomPost implementation
    By having $sample as the first pipeline operator, we use the pseudo-random behavior as documented in https://docs.mongodb.com/manual/reference/operator/aggregation/sample/
    which has far better performances for big databases.
    The drawback is the off-chance that we may end up with an empty result set. For this reason, we added 2 additional iterations, this should be an effective tradeoff for document sets with a limited number of "bad" documents

--- backstore/mongodb.go
+++ backstore/mongodb.go
@@ -2,23 +2,71 @@ package backstore
 
 import (
 	"context"
-	"time"
-
+	"errors"
 	"github.com/shitpostingio/autopostingbot/documentstore/entities"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
 )
 
 const opDeadline = 10 * time.Second
 
-func FindRandomPost(collection *mongo.Collection) (entities.Post, error) {
+//func FindRandomPost(collection *mongo.Collection) (entities.Post, error) {
+//
+//	ctx, cancelCtx := context.WithTimeout(context.Background(), opDeadline)
+//	defer cancelCtx()
+//
+//	//
+//	pipeline := mongo.Pipeline{
+//		{
+//			{
+//				Key: "$match",
+//				Value: bson.D{
+//					{Key: "haserror", Value: nil},
+//					{Key: "deletedat", Value: nil},
+//					{Key: "postedat", Value: bson.D{{Key: "$exists", Value: true}}},
+//				},
+//			},
+//		},
+//		{
+//			{
+//				Key: "$sample",
+//				Value: bson.D{
+//					{Key: "size", Value: 1},
+//				},
+//			},
+//		},
+//	}
+//
+//	//
+//	cursor, err := collection.Aggregate(ctx, pipeline, options.Aggregate())
+//	if err != nil {
+//		return entities.Post{}, err
+//	}
+//
+//	var posts []entities.Post
+//	err = cursor.All(ctx, &posts)
+//	if err != nil || len(posts) == 0 {
+//		return entities.Post{}, err
+//	}
+//
+//	return posts[0], err
+//
+//}
 
-	ctx, cancelCtx := context.WithTimeout(context.Background(), opDeadline)
-	defer cancelCtx()
+func FindRandomPost(collection *mongo.Collection) (entities.Post, error) {
 
 	//
 	pipeline := mongo.Pipeline{
+		{
+			{
+				Key: "$sample",
+				Value: bson.D{
+					{Key: "size", Value: 1},
+				},
+			},
+		},
 		{
 			{
 				Key: "$match",
@@ -29,28 +77,26 @@ func FindRandomPost(collection *mongo.Collection) (entities.Post, error) {
 				},
 			},
 		},
-		{
-			{
-				Key: "$sample",
-				Value: bson.D{
-					{Key: "size", Value: 1},
-				},
-			},
-		},
 	}
 
-	//
-	cursor, err := collection.Aggregate(ctx, pipeline, options.Aggregate())
-	if err != nil {
-		return entities.Post{}, err
+	for i := 0; i < 3; i++ {
+
+		//
+		cursor, err := collection.Aggregate(context.TODO(), pipeline, options.Aggregate())
+		if err != nil {
+			return entities.Post{}, err
+		}
+
+		var posts []entities.Post
+		err = cursor.All(context.TODO(), &posts)
+		if err != nil || len(posts) == 0 {
+			continue
+		}
+
+		return posts[0], err
+
 	}
 
-	var posts []entities.Post
-	err = cursor.All(ctx, &posts)
-	if err != nil || len(posts) == 0 {
-		return entities.Post{}, err
-	}
-
-	return posts[0], err
+	return entities.Post{}, errors.New("no post found, try again")
 
 }
